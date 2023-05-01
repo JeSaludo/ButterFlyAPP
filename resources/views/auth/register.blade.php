8fe46f38228cb9e53079fcd383d2472e07de407d
Eris <janerissaludo@gmail.com>
2023-05-01 15:37:50 +0800

    added changes to iu

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@include("layout.header")
+@include("layout.header", ['title' => "Register"])
 @include("layout.nav")
 
 
@@ -13,7 +13,7 @@
                 <div class="grid grid-flow-col w-11/12 gap-4  ">
                     <div>
                         <label class=" block text-sm text-custom-white-p" for="firstName">First Name:</label>
-                        <input class="w-full block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                        <input class="w-full block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                         type="text" name="firstName" id="firstName" placeholder="Enter First Name">
                         @error('firstName')
                         <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
@@ -22,7 +22,7 @@
 
                     <div>
                         <label class="block text-sm text-custom-white-p" for="lastName">Last Name:</label>
-                        <input class="w-full block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                        <input class="w-full block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                         type="text" name="lastName" id="lastName" placeholder="Enter Last Name">
                         @error('lastName')
                         <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
@@ -31,37 +31,37 @@
                 </div>
 
                 <label class="my-2 block text-sm text-custom-white-p" for="wildlifePermit">WFP or WCP:
-                    <input class="w-11/12 block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="text" name="wildlifePermit" id="wildlifePermit" placeholder="Enter WCP or WFP"></label>
                     @error('wildlifePermit')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
                     @enderror
                 <label class="my-2 block text-sm text-custom-white-p" for="firstName">Business Name:
-                    <input class="w-11/12 block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="text" name="businessName" id="businessName" placeholder="Enter Business Name"></label>
                     @error('businessName')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
                     @enderror
                 <label class="my-2 block text-sm text-custom-white-p" for="ownerName">Owner Name:
-                    <input class="w-11/12 block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="text" name="ownerName" id="ownerName" placeholder="Enter Owner Name"></label>
                     @error('ownerName')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
                     @enderror
                 <label class="my-2 block text-sm text-custom-white-p" for="contact">Contact Number:
-                    <input class="w-11/12 block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="text" name="contact" id="contact" placeholder="Enter Contact Number"></label>
                     @error('contact')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
                     @enderror
                 <label class="my-2 block text-sm text-custom-white-p" for="address">Address:
-                    <input class="w-11/12 block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="text" name="address" id="address" placeholder="Enter Address"></label>
                     @error('address')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
                     @enderror
                 <label class="my-2 block text-sm text-custom-white-p" for="email">Email Address:
-                    <input class="w-11/12 block mt-2 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="email" name="email" id="email" placeholder="Enter Email Address"></label>
                     @error('email')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
@@ -85,5 +85,5 @@
 
 
 
-
+@include("layout.body-footer")
 @include("layout.footer")
