5f443804c6f673b662273f9c593b5302e4863820
Eris <janerissaludo@gmail.com>
2023-05-07 15:24:43 +0800

    added changes to format of app form

--- resources/views/admin/dashboard-app-form.blade.php
+++ resources/views/admin/dashboard-app-form.blade.php
@@ -10,32 +10,25 @@
                 <table class="w-full divide-y divide-gray-200">
                   <thead class="bg-gray-50">
                       <tr>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Full Name</th>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Address</th>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Transport to Address</th>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Purpose</th>
+                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Application ID</th>
+                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Application Name</th>
                           <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date of Transport</th>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Mode of Transport</th>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Butterfly Details</th>
-                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Quantity</th>
+                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Of Submission</th>
+                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
                       </tr>
                   </thead>
                   <tbody class="bg-white divide-y divide-gray-200">
                       @foreach($usersWithPermit as $user)
                           @foreach($user->applicationForms as $form)
                               <tr>
+                                  <td class="px-6 py-4">{{ $form->id }}</td>
                                   <td class="px-6 py-4">{{ $form->name }}</td>
-                                  <td class="px-6 py-4">{{ $form->address }}</td>
-                                  <td class="px-6 py-4">{{ $form->transport_address }}</td>
-                                  <td class="px-6 py-4">{{ $form->purpose }}</td>
                                   <td class="px-6 py-4">{{ $form->transport_date }}</td>
-                                  <td class="px-6 py-4">{{ $form->mode_of_transport }}</td>
+                                  <td class="px-6 py-4">{{ $form->created_at }}</td>
+                                  
                                   <td class="px-6 py-4">
-                                      <ul>
-                                          @foreach($form->butterflies as $butterfly)
-                                              <li>{{ $butterfly->name }} ({{ $butterfly->quantity }})</li>
-                                          @endforeach
-                                      </ul>
+                                      <a href="" class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Pending</a>
                                   </td>
                                   <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                     <a href="" class="mx-2 text-indigo-600 hover:text-indigo-900">View</a>
