58803d8446cf271f49c4e4bc616027ebbaaad2c6
Eris <janerissaludo@gmail.com>
2023-05-14 13:48:17 +0800

    added changes in crud

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 @include("layout.nav")
 
 
-<div class=" p-4 my-7 mx-10 xl:mx-48 bg-custom-dark-800 rounded-sm">
+<div class=" p-4 my-7 mx-10 xl:mx-48 bg-custom-dark-800 rounded-sm " >
     <div class="flex flex-col-reverse xl:flex-row">
         <div class="mx-auto w-10/12 p-2 xl:px-14 py-20">
             <h1 class="text-center xl:text-left  text-custom-dark-600 text-4xl font-lora font-bold">LOGIN <span class="text-white">TO ACCOUNT</span></h1>
@@ -11,7 +11,7 @@
             <form class="mt-5" action="/login/authenticate" method="post"> 
                 @csrf
                 <label class="my-2 block text-sm text-custom-white-p" for="email_or_username">Email or Username:
-                    <input class="w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
+                    <input class=" w-11/12 block mt-2 text-custom-dark-500 placeholder:text-custom-dark-500 bg-transparent border-custom-dark-500 border-2 p-1.5 rounded-md"
                     type="text" name="email_or_username" id="email_or_username" placeholder="Enter Email Address or Username"></label>
                     @error('email_or_username')
                     <div class="mt-2 text-red-700 font-roboto font-bold text-xs">{{ $message }}</div>
@@ -30,7 +30,7 @@
         
         </div>  
 
-        <div class="w-full">
+        <div class="">
             <img class=""  src="{{asset("images/register-img.png")}}" alt="" srcset="">
 
         </div>
